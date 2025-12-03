a455595610d1c9379a9ce810bb21ce447abd436e
AlexLiuDev233 <wzylin11@outlook.com>
2025-12-03 22:41:42 +0800

    manager: always pin metamodule in modules page's top (#650)
    directly cherry-picked from upstream commit 3630d670122a5652483b3816904d6bbf27a0db57
    
    Co-authored-by: weishu <twsxtd@gmail.com>
    Co-authored-by: YuKongA <70465933+YuKongA@users.noreply.github.com>
    Co-authored-by: Ylarod <me@ylarod.cn>

--- manager/app/src/main/java/com/sukisu/ultra/ui/viewmodel/ModuleViewModel.kt
+++ manager/app/src/main/java/com/sukisu/ultra/ui/viewmodel/ModuleViewModel.kt
@@ -101,8 +101,23 @@ class ModuleViewModel : ViewModel() {
     val moduleList by derivedStateOf {
         val comparator =
             compareBy<ModuleInfo>(
+                {
+                    val executable = it.hasWebUi || it.hasActionScript
+                    when {
+                        it.metamodule && it.enabled -> 0
+                        sortEnabledFirst && sortActionFirst -> when {
+                            it.enabled && executable -> 1
+                            it.enabled -> 2
+                            executable -> 3
+                            else -> 4
+                        }
+                        sortEnabledFirst && !sortActionFirst -> if (it.enabled) 1 else 2
+                        !sortEnabledFirst && sortActionFirst -> if (executable) 1 else 2
+                        else -> 1
+                    }
+                },
                 { if (sortEnabledFirst) !it.enabled else 0 },
-                { if (sortActionFirst) !it.hasWebUi && !it.hasActionScript else 0 },
+                { if (sortActionFirst) !(it.hasWebUi || it.hasActionScript) else 0 },
             ).thenBy(Collator.getInstance(Locale.getDefault()), ModuleInfo::id)
         modules.filter {
             it.id.contains(search, true) || it.name.contains(search, true) || HanziToPinyin.getInstance()
