3392344cb3d3b95618b66f0d767581b7367c32e5
Wang Han <416810799@qq.com>
2025-12-08 23:19:07 +0800

    kernel: Install manager fd in task work (#3057)
    Tracepoint context disables preemption, so we cannot install fd which
    can sleep.

--- kernel/setuid_hook.c
+++ kernel/setuid_hook.c
@@ -75,6 +75,12 @@ static inline bool is_allow_su()
     return ksu_is_allow_uid_for_current(current_uid().val);
 }
 
+static void ksu_install_manager_fd_tw_func(struct callback_head *cb)
+{
+    ksu_install_fd();
+    kfree(cb);
+}
+
 int ksu_handle_setresuid(uid_t ruid, uid_t euid, uid_t suid)
 {
     uid_t new_uid = ruid;
@@ -108,12 +114,20 @@ int ksu_handle_setresuid(uid_t ruid, uid_t euid, uid_t suid)
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 10, 0)
     if (ksu_get_manager_appid() == new_uid % PER_USER_RANGE) {
-        pr_info("install fd for manager: %d\n", new_uid);
-        ksu_install_fd();
         spin_lock_irq(&current->sighand->siglock);
         ksu_seccomp_allow_cache(current->seccomp.filter, __NR_reboot);
         ksu_set_task_tracepoint_flag(current);
         spin_unlock_irq(&current->sighand->siglock);
+
+        pr_info("install fd for manager: %d\n", new_uid);
+        struct callback_head *cb = kzalloc(sizeof(*cb), GFP_ATOMIC);
+        if (!cb)
+            return 0;
+        cb->func = ksu_install_manager_fd_tw_func;
+        if (task_work_add(current, cb, TWA_RESUME)) {
+            kfree(cb);
+            pr_warn("install manager fd add task_work failed\n");
+        }
         return 0;
     }
 
