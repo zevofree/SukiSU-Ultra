e7487b79f6b3ce0d901d4f88654933eebf32d0ed
Faris-at-Home <90097027+rsuntk@users.noreply.github.com>
2025-12-18 19:22:07 +0700

    kernel: setuid_hook: Remove unused includes and function (#3072)
    * is_allow_su is not needed anymore, since its used for validate prctl
    checking (cmiiw)
    * setuid_hook have so many unused headers, just include what we need
    
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- kernel/setuid_hook.c
+++ kernel/setuid_hook.c
@@ -1,35 +1,16 @@
 #include <linux/compiler.h>
-#include <linux/sched/signal.h>
+#include <linux/version.h>
 #include <linux/slab.h>
 #include <linux/task_work.h>
 #include <linux/thread_info.h>
 #include <linux/seccomp.h>
-#include <linux/bpf.h>
-#include <linux/capability.h>
-#include <linux/cred.h>
-#include <linux/dcache.h>
-#include <linux/err.h>
-#include <linux/fs.h>
-#include <linux/init.h>
-#include <linux/init_task.h>
-#include <linux/kernel.h>
-#include <linux/kprobes.h>
-#include <linux/mm.h>
-#include <linux/mount.h>
-#include <linux/namei.h>
-#include <linux/nsproxy.h>
-#include <linux/path.h>
 #include <linux/printk.h>
 #include <linux/sched.h>
-#include <linux/security.h>
-#include <linux/stddef.h>
+#include <linux/sched/signal.h>
 #include <linux/string.h>
 #include <linux/types.h>
 #include <linux/uaccess.h>
 #include <linux/uidgid.h>
-#include <linux/version.h>
-#include <linux/binfmts.h>
-#include <linux/tty.h>
 
 #include "allowlist.h"
 #include "setuid_hook.h"
@@ -41,7 +22,6 @@
 #include "supercalls.h"
 #include "syscall_hook_manager.h"
 #include "kernel_umount.h"
-#include "app_profile.h"
 
 static bool ksu_enhanced_security_enabled = false;
 
@@ -65,15 +45,6 @@ static const struct ksu_feature_handler enhanced_security_handler = {
     .get_handler = enhanced_security_feature_get,
     .set_handler = enhanced_security_feature_set,
 };
-
-static inline bool is_allow_su()
-{
-    if (is_manager()) {
-        // we are manager, allow!
-        return true;
-    }
-    return ksu_is_allow_uid_for_current(current_uid().val);
-}
 
 static void ksu_install_manager_fd_tw_func(struct callback_head *cb)
 {
@@ -113,7 +84,6 @@ int ksu_handle_setresuid(uid_t ruid, uid_t euid, uid_t suid)
         return 0;
     }
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 10, 0)
     if (ksu_get_manager_appid() == new_uid % PER_USER_RANGE) {
         spin_lock_irq(&current->sighand->siglock);
         ksu_seccomp_allow_cache(current->seccomp.filter, __NR_reboot);
@@ -143,27 +113,6 @@ int ksu_handle_setresuid(uid_t ruid, uid_t euid, uid_t suid)
     } else {
         ksu_clear_task_tracepoint_flag_if_needed(current);
     }
-#else
-    if (ksu_is_allow_uid_for_current(new_uid)) {
-        spin_lock_irq(&current->sighand->siglock);
-        disable_seccomp();
-        spin_unlock_irq(&current->sighand->siglock);
-
-        if (ksu_get_manager_appid() == new_uid % PER_USER_RANGE) {
-            pr_info("install fd for manager: %d\n", new_uid);
-            struct callback_head *cb = kzalloc(sizeof(*cb), GFP_ATOMIC);
-            if (!cb)
-                return 0;
-            cb->func = ksu_install_manager_fd_tw_func;
-            if (task_work_add(current, cb, TWA_RESUME)) {
-                kfree(cb);
-                pr_warn("install manager fd add task_work failed\n");
-            }
-        }
-
-        return 0;
-    }
-#endif
 
     // Handle kernel umount
     ksu_handle_umount(old_uid, new_uid);
